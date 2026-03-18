9accbf1992bcb258930c581880c124f22c3007d9
LonchaDeXope <142729532+LonchaDeXope@users.noreply.github.com>
2026-03-18 14:14:29 +0100

    biciiii

--- Assets/AlexScripts/JumpOnBike.cs
+++ Assets/AlexScripts/JumpOnBike.cs
@@ -6,21 +6,22 @@ public class JumpOnBike : MonoBehaviour
 
     public GameObject bici;
     public GameObject player;
+    public GameObject player2;
 
     public CharacterController characterController;
 
-    private void Update()
+    private void Start()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        player.transform.localPosition = new Vector3(0.18f, -0.4f, 0f);
+
+       // StartCoroutine(MountPlayer());
     }
+
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
 
-            characterController.enabled = false;
 
             StartCoroutine(MountPlayer());
             
@@ -29,11 +30,15 @@ public class JumpOnBike : MonoBehaviour
 
     private IEnumerator MountPlayer()
     {
+        characterController.enabled = false;
         yield return new WaitForSeconds(.1f);
 
-        player.transform.parent = bici.transform;
-        player.transform.localPosition = new Vector3(0.18f, -0.4f, -0f);
-        player.transform.localRotation = Quaternion.Euler(new Vector3(0f, 270f, 0f));
+        //player.transform.parent = bici.transform;
+        //player.transform.localPosition = new Vector3(0.18f, -0.4f, -0f);
+        //player.transform.localRotation = Quaternion.Euler(new Vector3(0f, 270f, 0f));
+
+        player.SetActive(false);
+        player2.SetActive(true);
 
         Destroy(gameObject);
     }
