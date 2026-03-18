484d0ac7b85311cdd7712f5c09e0f7595efa1343
AlvaroQ <alvaro.quintanilla@outlook.es>
2026-03-18 20:34:37 +0100

    Cambios finales 1

---
+++ Assets/Prefabs/Demogorgon bueno/TriggerTeleportAction.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class TriggerTeleportAction : MonoBehaviour
+{
+    public Transform character;
+    public Transform targetPoint;
+    private AudioSource audioSource;
+    private Animator animator;
+    private bool activated = false;
+
+    void Start()
+    {
+        if (character != null)
+        {
+            animator = character.GetComponent<Animator>();
+            audioSource = character.GetComponent<AudioSource>();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (activated) return;
+
+        if (other.tag != "Player") return;
+        character.position = targetPoint.position;
+
+        //character.rotation = targetPoint.rotation;
+
+        if (animator != null)
+        {
+            animator.SetTrigger("Action");
+        }
+
+        if (audioSource != null)
+        {
+            audioSource.Play();
+        }
+        activated = true;
+    }
+}
